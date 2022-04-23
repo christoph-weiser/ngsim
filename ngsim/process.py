2adb116ebfde26b3591aeb6cdbe2ace6101390f1
chrische <christoph-weiser@gmx.de>
2022-04-23 23:23:04 +0200

    setting dummy_ignore variable when calling xschem

--- ngsim/process.py
+++ ngsim/process.py
@@ -40,7 +40,7 @@ def xschem2spice(schematic, outputdir, xschemrc):
     if outputdir == ".":
         outputdir = pwd
     p = subprocess.Popen(["xschem", "-x", "-n", "-q", schematic,
-                          "-o", outputdir, "--tcl", "set cmdline_ignore true",
+                          "-o", outputdir, "--tcl", "set cmdline_ignore true; set dummy_ignore true",
                           "--rcfile", xschemrc])
     (output, err) = p.communicate()
     p_status = p.wait()
